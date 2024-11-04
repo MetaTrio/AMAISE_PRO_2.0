f188f3423b204410ce611e693ce2e2ad42254169
ChadmiRatnayake <chadmi.20@cse.mrt.ac.lk>
2024-11-04 14:38:21 +0000

    Save the predicted probilities along with predicted labels

--- Binary_FFNN/binary_classification.py
+++ Binary_FFNN/binary_classification.py
@@ -82,7 +82,7 @@ def main(input_fasta_fastq, type_, input_kmers, model, output):
     k_mer_arr = pd.read_csv(k_mers, header=None).to_numpy()
     # input_data = [np.reshape(row.astype(np.float32), (32,)) for row in k_mer_arr]  # 3mer
     # input_data = [np.reshape(row.astype(np.float32), (136,)) for row in k_mer_arr]  # 4mer
-    input_data = [np.reshape(row.astype(np.float32), (40,)) for row in k_mer_arr]  # 2mer, 3mer
+    input_data = [np.reshape(row.astype(np.float32), (42,)) for row in k_mer_arr]  # 2mer, 3mer
     
     accession_numbers = [seq.id for seq in SeqIO.parse(inputset, type_)]
     
@@ -105,6 +105,7 @@ def main(input_fasta_fastq, type_, input_kmers, model, output):
     
     # Prediction
     predictions = []
+    prdicted_probabilities = []
     # with torch.no_grad():
     #     for data in input_data:
     #         x = torch.tensor(data).to(device)
@@ -119,17 +120,24 @@ def main(input_fasta_fastq, type_, input_kmers, model, output):
 
 
     # Threshold for binary output
-    predicted_labels = [1 if p > 0.5 else 0 for p in predictions]
+    # predicted_labels = [1 if p > 0.5 else 0 for p in predictions]
+
+    # Prediction with labels and probabilities in one loop
+    predicted_labels_probs = [(1 if p > 0.5 else 0, float(p)) for p in predictions]
+
+    # Unpack labels and probabilities for DataFrame creation
+    predicted_labels, probabilities = zip(*predicted_labels_probs)
+
     elapsed_time = time.time() - start_time
     memory_after = psutil.Process().memory_info()
     
     # Log time and memory
     logger.info(f"Total time for predictions: {elapsed_time / 60:.2f} minutes")
-    logger.info(f"Memory usage before predictions: {memory_before.rss / (1024 * 1024):.2f} MB")
-    logger.info(f"Memory usage after predictions: {memory_after.rss / (1024 * 1024):.2f} MB")
+    logger.info(f"Memory usage before predictions: {memory_before.rss / (1024 * 1024 * 1024):.2f} GB")
+    logger.info(f"Memory usage after predictions: {memory_after.rss / (1024 * 1024 * 1024):.2f} GB")
     
     # Output predictions to file
-    prediction_data = pd.DataFrame({"id": accession_numbers, "pred_label": predicted_labels})
+    prediction_data = pd.DataFrame({"id": accession_numbers, "pred_label": predicted_labels, "pred_prob": probabilities})
     prediction_data.to_csv(f"{resultPath}predictions.csv", index=False)
     logger.info("Predictions saved successfully.")
 
